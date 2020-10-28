b8e748c67deda481b0deee4bfa64e73a8b70e92a
Vasil Yordanov <vasil.yordanov@gmail.com>
2020-10-28 06:34:16 +0200

    Update dmc.m

--- dmc.m
+++ dmc.m
@@ -1,46 +1,43 @@
 clear;
-n=1;
-d=3;
 N0=10000;
-x=20*rand(n*d,N0)-10;
+x=20*rand(3,N0)-10;
 E_R=0;
-T=200;
+T=50;
 dt_scalar=0.05;
 N_T=T/dt_scalar;
 dt=ones(N_T,1)*dt_scalar;
-figure(2);
+figure(1);
+N=N0;
 for i=1:N_T
-    N=size(x,2);
+    % diffusion - random walk
     x = x+sqrt(dt(i))*randn(3,N);
+
+    % calculate potential
     V = -1./sqrt(sum(x.^2));
     rc=-3/(2*(-log(N0+1)/dt(i)+E_R));
     Vrc=-2./(3*rc);
     V(rc>sqrt(sum(x.^2))) = Vrc;
 
-    fprintf('avg energy: %f\n',sum(V)/N);
     P = exp( -( V - E_R )*dt(i));
-    d_idx = (V > E_R) & 1 - P > rand(1,N);
-    fprintf('will kill %d\n', sum(d_idx));
+    
     % kill
+    d_idx = (V > E_R) & 1 - P > rand(1,N);
     x(:,d_idx) = [];
     V(d_idx) = [];
-    P(d_idx) = [];
-    fprintf('after kill, avg energy: %f, E_R=%f\n',sum(V)/N, E_R);
-        
+    P(d_idx) = [];    
     N=size(x,2);
+    
+    % born
     m = fix(P - 1 + rand(1,N));
     b_idx = V < E_R;
-    % born
-    fprintf('will born: %d\n',sum(b_idx .* m)); 
-
     x=repelem(x, 1, b_idx .* m + 1);
-    N = size(x,2)
-    fprintf('after born, avg energy: %f\n',sum(V)/N);
+    N = size(x,2);
+
+    % calculate the new reference energy
     V_avg = sum(V)/N;
     E_R = V_avg-(N-N0)/(N0*dt(i));
+    fprintf('V_avg: %.5f, E_R=%.5f, N=%.5f\n', V_avg, E_R, N);
         
-    plot(i,E_R,'ro');
+    plot(i,E_R,'r.');
     hold on;
-    xlim([0 N_T]);
-    ylim([-1 0]);
 end
